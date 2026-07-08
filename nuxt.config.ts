718d4e6665a2abb28c7765ea972def3f650cc138
nbnotabene <niels@bjerre.net>
2026-07-08 23:47:56 +0200

    nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,22 +2,35 @@ import Aura from '@primeuix/themes/aura'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxtjs/tailwindcss',
-    '@primevue/nuxt-module'
-  ],
-  primevue: {
-    options: {
-      theme: {
-        preset: Aura // Other options include Lara or Nora from @primeuix/themes
-      }
-    }
-  },
-  // This enforces Static Site Generation (SSG) mode
-  ssr: true,
-  nitro: {
-    static: true
-  }
+	compatibilityDate: '2025-07-15',
+	devServer: {
+		host: '192.168.1.71', // or a specific intranet IP
+		port: 3000
+	},
+	devtools: { enabled: true },
+
+	modules: [
+		'@nuxtjs/tailwindcss',
+		'@primevue/nuxt-module'
+	],
+	primevue: {
+		options: {
+			theme: {
+				preset: Aura // Other options include Lara or Nora from @primeuix/themes
+			}
+		}
+	},
+	vite: {
+		optimizeDeps: {
+			include: [
+				'@vue/devtools-core',
+				'@vue/devtools-kit',
+			]
+		}
+	},
+	// This enforces Static Site Generation (SSG) mode
+	ssr: true,
+	nitro: {
+		static: true
+	}
 })
